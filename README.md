bcaaa5a9d482c4c845002b06aae6faed9deade79
hana <asufanah@gmail.com>
2015-04-05 20:23:51 +0900

    Update README.

--- README.md
+++ README.md
@@ -4,21 +4,39 @@
 
 [![Build Status](https://travis-ci.org/asufana/WAFLite.svg?branch=master)](https://travis-ci.org/asufana/WAFLite)
 
-Tiny Web Application Framework for Java8.
-
+A Sinatra-like simple Web Application Framework for Java8.
 
 ## Examples
+
+#### Application code
+
+```java
+public class WebApp extends WAFLite {
+    
+    public WebApp(final Integer port) {
+        super(port);
+    }
+    
+    @Override
+    public Server start() {
+        
+        # set routing
+        get("/hoge", (req, res) -> res.render(String.format("Hello Hoge! ( Method: %s )", req.method())));
+        get("/moge", (req, res) -> res.render(String.format("Hello Moge! ( Method: %s )", req.method())));
+        
+        return super.start();
+    }
+}
+```
+
+#### Test code
 
 ```java
 @Test
 public void test() throws Exception {
 
     final Integer port = 8080;
-    final WAFLite waf = new WAFLite(port);
-    waf.get("/hoge", (req, res) -> res.render(String.format("Hello Hoge! ( Method: %s )", req.method())));
-    waf.get("/moge", (req, res) -> res.render(String.format("Hello Moge! ( Method: %s )", req.method())));
-
-    final Server server = waf.start();
+    final Server server = new WebApp(port).start();
     
     final HttpResponse res01 = Http.get(port, "/hoge");
     assertThat(res01.code(), is(200));
--- src/test/java/com/github/asufana/waf/WAFLiteTest.java
+++ src/test/java/com/github/asufana/waf/WAFLiteTest.java
@@ -62,4 +62,41 @@ public class WAFLiteTest {
         
         server.stop();
     }
+    
+    //------------------------------------
+    
+    @Test
+    public void testSubClass() throws Exception {
+        final Server server = new WebApp(port).start();
+        
+        final HttpResponse res01 = Http.get(port, "/hoge");
+        assertThat(res01.code(), is(200));
+        assertThat(res01.contents(), is("Hello Hoge! ( Method: GET )"));
+        
+        final HttpResponse res02 = Http.get(port, "/moge");
+        assertThat(res02.code(), is(200));
+        assertThat(res02.contents(), is("Hello Moge! ( Method: GET )"));
+        
+        server.stop();
+    }
+    
+    public static class WebApp extends WAFLite {
+        
+        public WebApp(final Integer port) {
+            super(port);
+        }
+        
+        @Override
+        public Server start() {
+            get("/hoge",
+                (req, res) -> res.render(String.format("Hello Hoge! ( Method: %s )",
+                                                       req.method())));
+            get("/moge",
+                (req, res) -> res.render(String.format("Hello Moge! ( Method: %s )",
+                                                       req.method())));
+            
+            return super.start();
+        }
+    }
+    
 }
